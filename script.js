06bfe939e2d9218f11b371bf6a51c043bcab6259
Murtaza <me@murfasa.com>
2025-07-26 16:01:09 -0400

    Update hero

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
+let vantaEffect;
+
+function initVantaEffect() {
+  // Destroy existing effect if it exists
+  if (vantaEffect) {
+    vantaEffect.destroy(); 
+  };
+
+  // Initialize Vanta effect
+  vantaEffect = VANTA.CLOUDS({
+    el: "#hero",
+    mouseControls: false,
+    touchControls: false,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    skyColor: document.documentElement.classList.contains("dark") ? 0x1a1a2e : 0x68b8d7,
+    cloudColor: 0xadc1de,
+    cloudShadowColor: 0x183550,
+    sunColor: document.documentElement.classList.contains("dark") ? 0x000000 : 0xff9919,
+    sunlightColor: document.documentElement.classList.contains("dark") ? 0x000000 : 0xff9933,
+    sunGlareColor: document.documentElement.classList.contains("dark") ? 0x000000 : 0xff6633,
+    speed: 1
+  });
+}
+
+
 function setDarkMode() {
   const prefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
   if (prefersDark) {
@@ -22,6 +49,8 @@ function hideLoader() {
 // Toggle dark mode function for UI
 function toggleDarkMode() {
   document.documentElement.classList.toggle("dark");
+
+  initVantaEffect();
 }
 
 function updateCopyrightYear() {
@@ -55,6 +84,8 @@ function waitForResources() {
 // Wait for resources to load before hiding loader
 document.addEventListener('DOMContentLoaded', () => {
   updateCopyrightYear();
+  initVantaEffect();
+
   waitForResources().then(hideLoader());
   // Fallback
   setTimeout(hideLoader, 5000);
